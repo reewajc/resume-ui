511022625cd3072db35ae71c040693812f4d534a
Ram Chapagai <ramchapagai@Rams-MacBook-Pro.local>
2024-05-17 18:52:13 -0400

    fix font

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,8 +15,11 @@ const greeting = hour < 12 ? 'Good Morning' : (hour < 18 ? 'Good Afternoon' : 'G
       <div className="greeting-container">
         <h1 className="greeting">👋🏻 {greeting}</h1>
         <p className='about-me'>
-          Hi, I am  Ram ! I am a passionate Software Engineer with experience in AWS and Web Development. Welcome to my resume
-          website. Here, you can learn more about my background, skills, and projects.
+          Hi, I am  Ram ! I am a passionate Software Engineer with experience in AWS and Web Development. Someone who is very
+          easy to work with. I am a quick learner and I am always looking for new challenges.
+          Welcome to my resume website. Here, you can learn more about my background, skills, and projects.
+          Feel free to contact me if you have any questions or would like to collaborate on a project.
+          Thank you for visiting my resume website.
         </p>
         <span>
           <Certification/>
